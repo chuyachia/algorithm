b30c54fd964052fbbc0f7fdeb20fd12813df1ef7
Chu-Ya Chia <chuyachia@gmail.com>
2020-04-19 22:32:31 -0400

    Add FordFulkerson with BFS

--- src/main/java/graph/FordFulkerson.java
+++ src/main/java/graph/FordFulkerson.java
@@ -6,12 +6,14 @@ import java.util.List;
 
 public class FordFulkerson {
     private static class EdgeCapacity {
+        private final int from;
         private final int to;
         private final int capacity;
         private int flow;
         private EdgeCapacity residual;
 
-        public EdgeCapacity(int to, int capacity, int flow) {
+        public EdgeCapacity(int from , int to, int capacity, int flow) {
+            this.from = from;
             this.to = to;
             this.capacity = capacity;
             this.flow = flow;
@@ -51,7 +53,7 @@ public class FordFulkerson {
         List<Edge>[] graph = new List[11];
         graph[0] = Arrays.asList(new Edge(1, 7), new Edge(2, 2), new Edge(3, 1));
         graph[1] = Arrays.asList(new Edge(4, 2), new Edge(5, 4));
-        graph[2] = Arrays.asList(new Edge(5, 5));
+        graph[2] = Arrays.asList(new Edge(5, 5), new Edge(6, 6));
         graph[3] = Arrays.asList(new Edge(4, 4), new Edge(8, 8));
         graph[4] = Arrays.asList(new Edge(7, 7), new Edge(8, 1));
         graph[5] = Arrays.asList(new Edge(7, 3), new Edge(6, 8), new Edge(9, 3));
@@ -67,9 +69,58 @@ public class FordFulkerson {
 
     public int computeMaxFlow() {
         setupResidualGraph();
-        searchPath();
+//        searchPath();
+        searchPathBFS();
 
         return maxFlow;
+    }
+
+    private void searchPathBFS() {
+        for (int flow = bfs(); flow > 0; flow = bfs()) {
+            Arrays.fill(visited, false);
+            maxFlow += flow;
+        }
+    }
+
+    private int bfs() {
+        LinkedList<EdgeCapacity> queue = new LinkedList<>();
+        int[] minFlow = new int[graph.length];
+        EdgeCapacity[] linkedEdge = new EdgeCapacity[graph.length];
+        minFlow[source] = Integer.MAX_VALUE;
+        linkedEdge[source] = null;
+        visited[source] = true;
+
+        for (EdgeCapacity nextEdge : residualGraph[source]) {
+            if (nextEdge.remainingCapacity() > 0) {
+                queue.add(nextEdge);
+            }
+        }
+
+        while (queue.size() > 0) {
+            EdgeCapacity edge = queue.poll();
+            visited[edge.to] = true;
+            minFlow[edge.to] = Math.min(minFlow[edge.from], edge.remainingCapacity());
+            linkedEdge[edge.to] = edge;
+
+            if (edge.to == sink) break;
+
+            if (residualGraph[edge.to] == null) continue;
+
+            for (EdgeCapacity nextEdge : residualGraph[edge.to]) {
+                if (!visited[nextEdge.to] && nextEdge.remainingCapacity() > 0) {
+                    queue.add(nextEdge);
+                    visited[nextEdge.to] = true;
+                }
+            }
+        }
+
+        EdgeCapacity currentTracedEdge = linkedEdge[sink];
+        while (currentTracedEdge != null) {
+            pushFlow(currentTracedEdge, minFlow[sink]);
+            currentTracedEdge = linkedEdge[currentTracedEdge.from];
+        }
+
+        return minFlow[sink];
     }
 
     private void searchPath() {
@@ -117,8 +168,8 @@ public class FordFulkerson {
             if (inputGraphEdges == null) continue;
 
             for (Edge edge : inputGraphEdges) {
-                EdgeCapacity newEdge = new EdgeCapacity(edge.getVertex(), edge.getWeight(), 0);
-                EdgeCapacity newResidualEdge = new EdgeCapacity( i, 0, 0);
+                EdgeCapacity newEdge = new EdgeCapacity(i, edge.getVertex(), edge.getWeight(), 0);
+                EdgeCapacity newResidualEdge = new EdgeCapacity(edge.getVertex(), i, 0, 0);
                 newEdge.setResidual(newResidualEdge);
                 newResidualEdge.setResidual(newEdge);
 
